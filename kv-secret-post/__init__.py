ba93962b70d09aa289ea4bbc10ed90e40863cce0
Reza Mubeen <rmubeen@hitachisolutions.com>
2023-06-10 18:36:40 -0500

    add get -service

--- kv-secret-post/__init__.py
+++ kv-secret-post/__init__.py
@@ -1,11 +1,16 @@
 import logging, json
 
 import azure.functions as func
-from services.todo_service import get_todos
+from services.kv_secrets_service import get_kv_secret_by_key
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    data = get_todos()
+    try:
+        req_body = req.get_json()
+    except ValueError:
+        pass
+    data = get_kv_secret_by_key(req_body.get('key'))
+
     return func.HttpResponse(json.dumps(data), headers={"content-type": "application/json"})
