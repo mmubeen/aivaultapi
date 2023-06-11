3dfe52cf704ad2833bcb3f833693d7792671dc0e
Reza Mubeen <rmubeen@hitachisolutions.com>
2023-06-11 01:44:53 -0500

    add get -service

--- services/kv_secrets_service.py
+++ services/kv_secrets_service.py
@@ -1,10 +1,10 @@
 from azure.core.exceptions import ResourceNotFoundError
 from azure.identity import DefaultAzureCredential
-from azure.keyvault.secrets import SecretClient
+from azure.keyvault.secrets import SecretClient, KeyVaultSecret
 import os
 
 
-def get_kv_secret_by_key(secret_key):
+def get_kv_secret_by_key(secret_key) -> str:
     credential = DefaultAzureCredential()
     key_vault_url = os.getenv("KEY_VAULT_URL")
 
@@ -14,7 +14,7 @@ def get_kv_secret_by_key(secret_key):
         raise Exception(f'Key vault <${key_vault_url}> not found ')
 
     try:
-        secret = secret_client.get_secret(secret_key)
+        key_vault_secret: KeyVaultSecret = secret_client.get_secret(secret_key)
     except ResourceNotFoundError as e:
         raise Exception(f'Key vault value for <${secret_key}> not found ')
-    return secret
+    return key_vault_secret.value
