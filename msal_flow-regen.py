b9a745e4d8fa3e1346dd85a5447a0656df4f3247
Reza Mubeen <rmubeen@hitachisolutions.com>
2023-06-15 13:04:50 -0500

    add get -service

--- msal_flow-regen.py
+++ msal_flow-regen.py
@@ -4,22 +4,21 @@ import requests
 # If the caller is not already signed in to Azure, the caller's
 # web browser will prompt the caller to sign in first.
 
-# pip install msal
-from msal import PublicClientApplication, ConfidentialClientApplication, ClientApplication
+from msal import ClientApplication
 import sys
 
 # You can hard-code the registered app's client ID and tenant ID here,
 # or you can provide them as command-line arguments to this script.
 client_id = '955373bc-d80f-4b35-a792-22a8537c059b'
 tenant_id = '3e9aadf8-6a16-490f-8dcd-c68860caae0b'
-client_secret = "2B08Q~Qa7mE-IrqkS6HyuC9mPpYm01s60u6YnaoD"
-application_id = '18edded3-3130-4d99-9ce0-f88dfaa04ebf'
+client_secret = "TUD8Q~-VbYwGOd4M3J.-9kVUqTn-71E_QdSBVc8D"
+application_id = '955373bc-d80f-4b35-a792-22a8537c059b'
 
-endpoint = 'https://ecf-prod-openai-apim-001.azure-api.net/GetKeyVaultSecret-ECF-PROD/aikey'
-ai_endpoint = 'https://ecf-prod-openai-apim-001.azure-api.net/openai/deployments/apim-test-deployment/completions?api-version=2022-12-01'
+endpoint = 'https://regn-eastus-ga-prod-apim-ai-001.azure-api.net/REGN-EastUS-GA-Prod-FA-AI-001/aikey'
+ai_endpoint = 'https://regn-eastus-ga-prod-apim-ai-001.azure-api.net/v1/completions'
 # Do not modify this variable. It represents the programmatic ID for
 # Azure Databricks along with the default scope of '/.default'.
-scopes = ['api://18edded3-3130-4d99-9ce0-f88dfaa04ebf/ApiUser.Read']
+scopes = ['api://955373bc-d80f-4b35-a792-22a8537c059b/ApiUser.Read']
 
 # Check for too few or too many command-line arguments.
 if (len(sys.argv) > 1) and (len(sys.argv) != 3):
@@ -34,33 +33,22 @@ if len(sys.argv) > 1:
 
 app = ClientApplication(
     client_id=client_id,
-    authority="https://login.microsoftonline.com/" + tenant_id,
-    client_credential=client_secret
+    authority="https://login.microsoftonline.com/" + tenant_id
 )
 
-acquire_tokens_result = app.acquire_token_by_username_password(username='admin_openai@ecfdata.com',
-                                                               password='o1@F}93BMc4.)uq',
+acquire_tokens_result = app.acquire_token_by_username_password(username='svc.dte-azopenai@regeneron.com',
+                                                               password='pzW3QrAm4?*,PQ7n',
                                                                scopes=scopes)
 
 if 'error' in acquire_tokens_result:
     print("Error: " + acquire_tokens_result['error'])
     print("Description: " + acquire_tokens_result['error_description'])
 else:
-    # print("Access token:\n")
-    # print(acquire_tokens_result['access_token'])
-    # print("\nRefresh token:\n")
-    # print(acquire_tokens_result['refresh_token'])
-
     header_token = {"Authorization": "Bearer {}".format(acquire_tokens_result['access_token'])}
-    # rt = requests.get(url=endpoint, headers=header_token)
     rt = requests.post(url=endpoint, headers=header_token, data=b'{"key":"apimunlimitedkey1"}')
-    # print(rt.status_code)
-    print(rt.json())
 
-    request_data = b'{"prompt":"Write a product launch email for new AI-powered headphones that are priced at $79.99 and available at Best Buy, Target and Amazon.com. The target audience is tech-savvy music lovers and the tone is friendly and exciting.\n\n1. What should be the subject line of the email?  \n2. What should be the body of the email?","max_tokens":350,"temperature":1,"frequency_penalty":0,"presence_penalty":0,"top_p":1,"stop":null}'
-
-    ai_header_key = {"ocp-apim-subscription-key": "{}".format(rt.json())}
-
-    ai_rt = requests.post(url=endpoint, headers=ai_header_key, data=request_data)
+    request_data = b'{"prompt":"What should be the body of the email?","max_tokens":350,"temperature":1,"frequency_penalty":0,"presence_penalty":0,"top_p":1,"stop":null}'
+    ai_header_key = {"Ocp-Apim-Subscription-Key": "{}".format(rt.json())}
+    ai_rt = requests.post(url=ai_endpoint, headers=ai_header_key, data=request_data)
 
     print(ai_rt.json())
--- msal_flow_ai_regen.py
+++ msal_flow_ai_regen.py
@@ -10,16 +10,15 @@ import sys
 
 # You can hard-code the registered app's client ID and tenant ID here,
 # or you can provide them as command-line arguments to this script.
-client_id = '18edded3-3130-4d99-9ce0-f88dfaa04ebf'
-tenant_id = '05e03292-6ca6-4f26-8c66-4b1e0ce68037'
-client_secret = "2B08Q~Qa7mE-IrqkS6HyuC9mPpYm01s60u6YnaoD"
-application_id = '18edded3-3130-4d99-9ce0-f88dfaa04ebf'
+client_id = '955373bc-d80f-4b35-a792-22a8537c059b'
+tenant_id = '3e9aadf8-6a16-490f-8dcd-c68860caae0b'
+client_secret = "TUD8Q~-VbYwGOd4M3J.-9kVUqTn-71E_QdSBVc8D"
+application_id = '955373bc-d80f-4b35-a792-22a8537c059b'
 
-endpoint = 'https://ecf-prod-openai-apim-001.azure-api.net/clone/openai/deployments/apim-test-deployment/completions?api-version=2022-12-01'
-
+ai_endpoint = 'https://regn-eastus-ga-prod-apim-ai-001.azure-api.net/v2/completions'
 # Do not modify this variable. It represents the programmatic ID for
 # Azure Databricks along with the default scope of '/.default'.
-scopes = ['api://18edded3-3130-4d99-9ce0-f88dfaa04ebf/ApiUser.Read']
+scopes = ['api://955373bc-d80f-4b35-a792-22a8537c059b/ApiUser.Read']
 
 # Check for too few or too many command-line arguments.
 if (len(sys.argv) > 1) and (len(sys.argv) != 3):
@@ -34,25 +33,17 @@ if len(sys.argv) > 1:
 
 app = ClientApplication(
     client_id=client_id,
-    authority="https://login.microsoftonline.com/" + tenant_id,
-    client_credential=client_secret
+    authority="https://login.microsoftonline.com/" + tenant_id
 )
 
-acquire_tokens_result = app.acquire_token_by_username_password(username='admin_openai@ecfdata.com',
-                                                               password='o1@F}93BMc4.)uq',
+acquire_tokens_result = app.acquire_token_by_username_password(username='svc.dte-azopenai@regeneron.com',
+                                                               password='pzW3QrAm4?*,PQ7n',
                                                                scopes=scopes)
-request_data=b'{"prompt":"Write a product launch email for new AI-powered headphones that are priced at $79.99 and available at Best Buy, Target and Amazon.com. The target audience is tech-savvy music lovers and the tone is friendly and exciting.\n\n1. What should be the subject line of the email?  \n2. What should be the body of the email?","max_tokens":350,"temperature":1,"frequency_penalty":0,"presence_penalty":0,"top_p":1,"stop":null}'
+request_data=b'{"prompt":"What should be the body of the email?","max_tokens":350,"temperature":1,"frequency_penalty":0,"presence_penalty":0,"top_p":1,"stop":null}'
 if 'error' in acquire_tokens_result:
     print("Error: " + acquire_tokens_result['error'])
     print("Description: " + acquire_tokens_result['error_description'])
 else:
-    print("Access token:")
-    print(acquire_tokens_result['access_token'])
-    # print("\nRefresh token:\n")
-    # print(acquire_tokens_result['refresh_token'])
-
     header_token = {"Authorization": "Bearer {}".format(acquire_tokens_result['access_token'])}
-    # rt = requests.get(url=endpoint, headers=header_token)
-    rt = requests.post(url=endpoint, headers=header_token, data=request_data)
-    # print(rt.status_code)
+    rt = requests.post(url=ai_endpoint, headers=header_token, data=request_data)
     print(rt.json())
